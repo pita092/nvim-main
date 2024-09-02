95a34b472002b7fc3e5378e3d98ef3436b612c79
pita <itaymalka37@gmail.com>
2024-09-03 00:42:44 +0300

    s

--- init.lua
+++ init.lua
@@ -34,8 +34,9 @@
 --
 --
 
-vim.g.base46_cache = vim.fn.stdpath('data').. '/base46_cache/'
-
+vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46_cache/'
+local base46_config = require("base46")
+require("base46").setup(base46_config)
 
 vim.g.mapleader = " "
 
