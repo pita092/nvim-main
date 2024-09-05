ec0194e4b33b8340d0f08ca6d5868615eade39ae
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 01:51:40 +0300

    s

--- init.lua
+++ init.lua
@@ -33,8 +33,7 @@
 -- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠒⠦⠤⠤⠤⠶⠒⠒⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 --
 --
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
 
 vim.g.mapleader = " "
 
@@ -68,3 +67,4 @@ vim.schedule(function()
   require("pitavim.maps")
 end)
 require("pitavim.highlights")
+require("configs.nvconfig")
