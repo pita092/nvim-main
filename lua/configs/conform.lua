a05f61f62fd2b121e2596d893a566ea3a20c0c9d
pita <154020297+pita092@users.noreply.github.com>
2024-09-18 21:40:36 +0300

    s

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,11 +1,10 @@
-local options = {
+require("conform").setup({
 	formatters_by_ft = {
-		lua = { "stylua" },
 		lua = { "stylua", "luaformatter" },
 		c = { "clang_format" },
 		cpp = { "clang_format" },
 		go = { "gofumpt", "goimports_reviser", "golines" },
 	},
-}
+  })
 
-return options
+
