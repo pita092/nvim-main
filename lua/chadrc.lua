a5f946ea6498786f12c3eea9e38dd311f062f291
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 17:35:25 +0300

    s

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,4 +1,12 @@
 local M = {}
+M.base46 = {
+	integrations = {
+		"blankline",
+		"cmp",
+		"git",
+		-- more
+	},
+}
 M.ui = {
 	theme = "gruvbox",
 	telescope = { style = "borderless" }, -- borderless / bordered
