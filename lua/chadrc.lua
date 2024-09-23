59510ebf362a1a5e776ba626d01e501e98fecc0d
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 21:33:32 +0300

    s

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,3 +1,15 @@
+local function get_hl_fg(name)
+	local hl = vim.api.nvim_get_hl(0, { name = name })
+	return hl.fg
+end
+
+-- Function to set the foreground color of a highlight group
+local function set_hl_fg(name, fg)
+	local current_hl = vim.api.nvim_get_hl(0, { name = name })
+	current_hl.fg = fg
+	vim.api.nvim_set_hl(0, name, current_hl)
+end
+local normal_fg = get_hl_fg("Normal")
 local M = {}
 M.base46 = {
 	lsp = { signature = false },
@@ -13,6 +25,7 @@ M.ui = {
 	hl_override = {
 		LspReferenceText = {
 			bg = "NONE",
+			fg = normal_fg,
 			underline = false,
 		},
 		LspReferenceRead = {
