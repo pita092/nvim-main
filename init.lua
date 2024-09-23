f8e812308f8d75c0bbe9265ed8808c774f56f7ae
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 21:25:26 +0300

    s

--- init.lua
+++ init.lua
@@ -65,12 +65,4 @@ require("pitavim.scripts")
 vim.schedule(function()
 	require("pitavim.maps")
 end)
-vim.api.nvim_create_autocmd("BufReadPost", {
-	group = vim.api.nvim_create_augroup("Change_Illuminate", { clear = true }),
-	callback = function()
-		vim.cmd([[hi IlluminatedWordText cterm=underline gui=undercurl]])
-		vim.cmd([[hi IlluminatedWordRead cterm=underline gui=undercurl]])
-		vim.cmd([[hi IlluminatedWordWrite cterm=underline gui=undercurl]])
-	end,
-	desc = "Change Illuminate Highlights",
-})
+vim.api.nvim_set_hl(0, “FloatBorder”, {bg=NONE, fg=NONE})
