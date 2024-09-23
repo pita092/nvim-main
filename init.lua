791c8d38e27696bf3b7fa8351018c6f9e348d58e
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 20:44:39 +0300

    s

--- init.lua
+++ init.lua
@@ -58,6 +58,12 @@ require("lazy").setup({
 		import = "pitavim.plugins",
 	},
 }, lazy_stuff)
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		client.server_capabilities.semanticTokensProvider = nil
+	end,
+})
 
 require("configs")
 require("pitavim.cmds")
