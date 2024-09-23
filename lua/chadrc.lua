da8792f40daf469ba0b2a0819c8ad6239a9681be
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 17:41:28 +0300

    s

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,6 +15,14 @@ M.ui = {
 	tabufline = {
 		enabled = false,
 	},
+	statusline = {
+		theme = "vscode", -- default/vscode/vscode_colored/minimal
+		-- default/round/block/arrow separators work only for default statusline theme
+		-- round and block will work for minimal theme only
+		separator_style = "default",
+		order = nil,
+		modules = nil,
+	},
 }
 
 return M
