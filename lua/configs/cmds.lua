0c95a49cb372f403677b1d8ad65ee496d70f4913
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 18:06:45 +0300

    s

--- lua/configs/cmds.lua
+++ lua/configs/cmds.lua
@@ -91,6 +91,6 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 
 		-- You can call `try_lint` with a linter name or a list of names to always
 		-- run specific linters, independent of the `linters_by_ft` configuration
-		require("lint").try_lint("cspell")
+		require("lint").try_lint("luacheck")
 	end,
 })
